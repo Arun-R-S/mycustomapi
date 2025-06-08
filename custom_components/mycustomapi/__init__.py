f0aa5c7476314bc1418607af022fe9d98b780f50
Arun-R-S <arunrs0966@gmail.com>
2025-06-08 18:51:28 +0530

    included logger

--- custom_components/mycustomapi/__init__.py
+++ custom_components/mycustomapi/__init__.py
@@ -1,5 +1,9 @@
 from .api import MyCustomAPI
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
+    _LOGGER.info("Setting up MyCustomAPI")
     hass.http.register_view(MyCustomAPI())
     return True
