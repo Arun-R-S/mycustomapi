d4e838a1fad75b9e140b53e2a5100d4af5f95052
Arun-R-S <arunrs0966@gmail.com>
2025-06-08 19:46:50 +0530

    Revert "Added UI Configuration"
    This reverts commit 4c29f874651456b682e4f62d93f032a2cd463c51.

--- custom_components/mycustomapi/__init__.py
+++ custom_components/mycustomapi/__init__.py
@@ -1,21 +1,9 @@
 from .api import MyCustomAPI
-
 import logging
 
 _LOGGER = logging.getLogger(__name__)
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    # Optional: still needed if you have YAML setup
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Called when a user adds your integration via the UI
+async def async_setup(hass, config):
     _LOGGER.info("Setting up MyCustomAPI")
     hass.http.register_view(MyCustomAPI())
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Optional clean-up logic
     return True
