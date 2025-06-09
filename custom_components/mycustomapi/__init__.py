926c8d6f513f2752c79f2e9eb32776b660633ab6
Arun-R-S <arunrs0966@gmail.com>
2025-06-09 08:42:47 +0530

    Added Config Flow

--- custom_components/mycustomapi/__init__.py
+++ custom_components/mycustomapi/__init__.py
@@ -7,3 +7,10 @@ async def async_setup(hass, config):
     _LOGGER.info("Setting up MyCustomAPI")
     hass.http.register_view(MyCustomAPI())
     return True
+
+async def async_setup_entry(hass, entry):
+    return True
+
+
+async def async_remove_entry(hass, entry):
+    return True
